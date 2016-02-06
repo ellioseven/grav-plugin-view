f22bec10843e8f2f052d2ca6728b28a353080dc7
Elliot Mitchum <elliot.mitchum@ellioseven.com>
2016-02-07 00:22:37 +1100

    View filtering now happens in view.php

--- view.php
+++ view.php
@@ -11,12 +11,19 @@ class ViewPlugin extends Plugin
 {
 
     /**
-     * Assign listeners
+     * @var Page
+     */
+    private $target;
+
+    /**
+     * Implements 'getSubscribedEvents' event.
+     * - Assigns plugin listeners.
      *
      * @return array
      */
     public static function getSubscribedEvents()
     {
+        // Assign listeners.
         return [
             'onPluginsInitialized' => ['onPluginsInitialized', 0],
             'onGetPageTemplates' => ['onGetPageTemplates', 0],
@@ -26,52 +33,130 @@ class ViewPlugin extends Plugin
     }
 
     /**
-     * Initialize configuration
+     * Implements 'onPluginsInitialized' event.
+     * - Set plugin as active.
      */
     public function onPluginsInitialized()
     {
+        // Plugin always active.
         $this->active = true;
         return;
     }
 
     /**
-     * Add current directory to twig lookup paths.
+     * Implements 'onTwigTemplatePaths' event.
+     * - Add twig paths to instance.
      */
     public function onTwigTemplatePaths()
     {
+        // Add current directory to twig lookup paths.
         $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
     }
 
+    /**
+     * Implements 'onGetPageTemplates' event.
+     * - Add blueprints & templates to instance.
+     *
+     * @param $event
+     */
     public function onGetPageTemplates($event)
     {
         $types = $event->types;
         $locator = Grav::instance()['locator'];
+
+        // Set blueprints & templates.
         $types->scanBlueprints($locator->findResources('plugin://' . $this->name . '/blueprints'));
         $types->scanTemplates($locator->findResources('plugin://' . $this->name . '/templates'));
     }
 
     /**
-     * Set needed variables to display view.
+     * Implements 'onTwigPageVariables' event.
+     * - Set view vars to page header.
      *
      * @param $event
      */
     public function onTwigPageVariables($event)
     {
+        // Get variables.
         $page = $event['page'];
+        $config = $this->mergeConfig($page);
+        $header = $page->header();
 
-        // Get view from page or exit.
-        if (isset($page->header()->view)) {
-            $view = $page->header()->view;
-        } else {
+        // Exit if no view in page header.
+        if (!isset($header->view)) {
             return;
         }
 
-        // Parse and set params frontmatter if set and not default.
-        if (isset($view['params']) && $view['params']) {
-            $view['params'] = (array) YamlParser::parse($view['params']);
+        // Define view vars.
+        $view_header['template'] = $config['template'];
+        $view_header['items'] = $this->getItems($page);
+
+        // Set page header.
+        $page->modifyHeader('view', $view_header);
+    }
+
+    /**
+     * Get view items from page collection.
+     *
+     * @param $page
+     * @return mixed
+     */
+    private function getItems($page) {
+
+        // Get vars.
+        $view_header = $page->header()->view;
+        $params = isset($view_header['params']) ? $view_header['params'] : false;
+        $filter = isset($view_header['limit']) ? $view_header['limit'] : false;
+
+        // Parse params or set to default.
+        if ($params) {
+            $params = (array) YamlParser::parse($params);
+        } else {
+            $params = 'content';
         }
 
-        // Set modified header.
-        $page->modifyHeader('view', $view);
+        // Check if page root.
+        if ($view_header['page'] != '/') {
+
+            // Set the target page.
+            $this->target = $page->find($view_header['page']);
+
+            // Get the target page collection.
+            $items = $this->target->collection($params);
+
+            // Filter the page collection.
+            if ($items && $filter) {
+                $items = $items->filter(array($this, 'filter'));
+            }
+
+        } else {
+
+            // Get the page collection.
+            $items = $page->collection($params);
+
+        }
+
+        return $items;
+
     }
+
+    /**
+     * Filter view collection result.
+     *
+     * @param $value
+     * @param $key
+     * @return bool
+     */
+    public function filter($value, $key) {
+
+        // If key is not in target page collection, filter it from results.
+        $children = $this->target->children();
+        if ($children->offsetGet($key)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
